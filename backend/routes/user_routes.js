0b6385e0c383abd21163808d12dbf17a00de8814
shiv <shivkumarloharkar2002@gmail.com>
2024-08-22 17:29:00 +0530

    manage user backend done

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,15 +1,20 @@
 import express from "express"
-import { Getallusers, Updateuser, UserLogin, Userregister } from "../controller/User_controller.js"
+import { Deleteemployee, Getallusers, Updateuser, UserLogin, Userregister } from "../controller/User_controller.js"
  const UserRoutes= express.Router()
 
-//  localhost:5555/api/userroutes/create
+//  http://localhost:5555/api/userroutes/create
 UserRoutes.post("/create", Userregister)
 
-// localhost:5555/api/userroutes/login
+// http://localhost:5555/api/userroutes/login
 UserRoutes.post("/login",UserLogin)
 
+http://localhost:5555/api/userroutes/updateuser
 UserRoutes.put("/updateuser",Updateuser)
 
+// http://localhost:5555/api/userroutes/getallusers
 UserRoutes.get("/getallusers", Getallusers)
+
+// http://localhost:5555/api/userroutes/deleteuser/66c71964eaa7f9474c490fff
+UserRoutes.delete("/deleteuser/:id",Deleteemployee)
 
 export default UserRoutes
