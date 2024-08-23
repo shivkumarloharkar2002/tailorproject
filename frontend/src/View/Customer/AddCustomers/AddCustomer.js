2c26688a64c4d70e35e4f1fc778c27f558c7ca23
Bhoyate Dhanashree <bhoyatedhanashree5055@gmail.com>
2024-08-23 14:08:50 +0530

    Upload image

--- frontend/src/View/Customer/AddCustomers/AddCustomer.js
+++ frontend/src/View/Customer/AddCustomers/AddCustomer.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './AddCustomer.css'
 import { Link } from 'react-router-dom'
+import Header from '../../../Component/Header/Header'
 
 export default function AddCustomer() {
   return (
     <>
-    <header className='top_nav'></header>
+    <header className='top_nav'><Header/></header>
     <form className='customer_form'>
       <h1 className='form_head'>Customer Information</h1>
       <input type='text' placeholder='Customer Name' className='customer_input'/><br/>
+      <input type='text' placeholder='Customer E-mail' className='customer_input'/><br/>
       <input type='number' placeholder='Customer Phone' className='customer_input'/><br/>
       <input type='text' placeholder='Customer Address' className='customer_input'/><br/>
       <Link to='/selectcategory'>  <button className='customer_btn'>Submit</button></Link>
