20d17af0ca370803203295b01c7e138130f52c5f
Jacky2204 <jalindarbhapkar2@gmail.com>
2024-08-24 10:03:28 +0530

    invoice

--- frontend/src/Routers/Common_routes.js
+++ frontend/src/Routers/Common_routes.js
@@ -5,75 +5,70 @@ import SelectCategory from "../View/Customer/AddCustomers/SelectCategory";
 import SelectFabric from "../View/Customer/AddCustomers/SelectFabric";
 import Measurement from "../View/Customer/AddCustomers/Measurement";
 import Login from "../View/Login/Login";
-import Invoice from '../View/Invoice/Invoice'
+import Invoice from "../View/Invoice/Invoice";
 import ViewCustomer from "../View/Customer/ViewCustomers/ViewCustomer";
 import TodayOrder from "../View/TodayOrderPage/TodayOrder";
 import NextOrderingPage from "../Component/OrderCard/NextOrderingPage";
 import Home from "../View/Home/Home";
 
-
-
-
 const routes = createBrowserRouter([
   {
-   path:'/',
-   element:<Home/>
+    path: "/",
+    element: <Home />,
   },
-    {
-      path:'/customerinfo',
-      element:<AddCustomer/>
-    },
-    {
-      path:'/seecustomer_details',
-      element:<ViewCustomer/>
-    },
-    {
-      path:'/selectcategory',
-      element:<SelectCategory/>
-    },
-    {
-      path:'/choosefabric',
-      element:<SelectFabric/>
-    },
-    {
-      path:'/fabricform',
-      element:<FabricForm/>
-    },
-    {
-      path:'/measurement',
-      element:<Measurement/> 
-    },
-    {
-      path:'/order',
-      element:<TodayOrder/>
-    },
-    {
-      path:'/orderslip',
-      element:<NextOrderingPage/>
-    },
-    {
+  {
+    path: "/customerinfo",
+    element: <AddCustomer />,
+  },
+  {
+    path: "/seecustomer_details",
+    element: <ViewCustomer />,
+  },
+  {
+    path: "/selectcategory",
+    element: <SelectCategory />,
+  },
+  {
+    path: "/choosefabric",
+    element: <SelectFabric />,
+  },
+  {
+    path: "/fabricform",
+    element: <FabricForm />,
+  },
+  {
+    path: "/measurement",
+    element: <Measurement />,
+  },
+  {
+    path: "/order",
+    element: <TodayOrder />,
+  },
+  {
+    path: "/orderslip",
+    element: <NextOrderingPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 
-      path:'/login',
-      element:<Login/>
-    },
-    {
-      path: "/register",
-      element: <Register/>
-    },
-  
-    {
-      path: "/invoice",
-      element: <Invoice/>
-    },
- {
-  path:'/readymade',
-  element:<ReadyMade/>
- }
+  {
+    path: "/invoice",
+    element: <Invoice />,
+  },
+  {
+    path: "/readymade",
+    element: <ReadyMade />,
+  },
+]);
 
-  ])
-
-  function CommonRoutes(){
-    return <RouterProvider router={routes} />
-  }
+function CommonRoutes() {
+  return <RouterProvider router={routes} />;
+}
 
 export default CommonRoutes;
