378cac541a82ef5607106d2eec93e4f397af4801
Aniket <aniketsupekar2004@gmail.com>
2024-08-25 16:45:22 +0530

    responsive pages

--- frontend/src/View/About/About.js
+++ frontend/src/View/About/About.js
@@ -18,7 +18,7 @@ export default function About() {
           <p>Our goal is to provide the best products and services to our customers. The Tailor Shop is located on The Citadel campus between the Cadet Store and Dry Cleaning Services.The Tailor Shop issues all tailored uniform items to cadets and performs alterations to cadets uniforms. Services are also available to all faculty, staff, students, Alumni, and anyone who needs top notch tailoring services. Our full service shop offers alterations on both military uniforms and civilian clothing.</p>
         </div>
       </div>
-      <div className='abo'>
+      <div className='aboss'>
         <div className='abo-text-2'>
           <h1 className='heding'>Career</h1>
           <p>Tailors take on a range of duties and responsibilities to ensure the creation of well-fitting and stylish garments. A tailor’s duties and responsibilities may include:Sewing and measuring clothing Designing and creating clothes from scratch for a customer, such as suits, jackets, blazers, pants and more Fixing clothes that have been damaged or ripped Altering clothes to fit better, including making garments shorter, longer, tighter or looser Ironing and steaming clothes that are finished and ready to be picked up by customers </p>
--- frontend/src/View/Performance/Performance.js
+++ frontend/src/View/Performance/Performance.js
@@ -9,15 +9,40 @@ import goods from './goods.png';
 export default function Performance() {
   return (
     <div>
-      {/* <Header/> */}
-      <h2 className='cent'>Name :-</h2>
-      <h2 className='cent'>Email ID :-</h2>
-      <h2 className='cent'>Phone :-</h2>
-      <h2 className='cent'>Role :-</h2>
+      <Header/>
+      
       <div className='parfom'>
 
         <div className='hedi'>
             <p className='numh' >Today's Performance</p>
+        </div>
+        <div className='flexp'>
+            <div className='rpart'>
+                <img className='icon' src={sales}/>
+                <p className='num'>₹3,000</p>
+                <p className='numi'>Total Sales</p>
+            </div>
+            <div className='jpart'>
+                <img className='icon' src={wallet}/>
+                <p className='num'>₹1,400</p>
+                <p className='numi'>Payments Received</p>
+            </div>
+            <div className='ppart'>
+                <img className='icon' src={recev}/>
+                <p className='num'>3</p>
+                <p className='numi'>Orders Received</p>
+            </div>
+            <div className='npart'>
+                <img className='icon' src={goods}/>
+                <p className='num'>1</p>
+                <p className='numi'>Orders Delivered</p>
+            </div>
+        </div>
+      </div>
+      <div className='parfom'>
+
+        <div className='hedi'>
+            <p className='numh' >Total Performance</p>
         </div>
         <div className='flexp'>
             <div className='rpart'>
