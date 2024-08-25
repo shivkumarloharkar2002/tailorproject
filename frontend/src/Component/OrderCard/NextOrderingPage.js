605d5ece00e6e07796291ed0f1a57bdcca85abbe
kiranmadage <kiranmadage96@gmail.com>
2024-08-25 10:29:48 +0530

    todyas ordering page

--- frontend/src/Component/OrderCard/NextOrderingPage.js
+++ frontend/src/Component/OrderCard/NextOrderingPage.js
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NextOrderingpage.css'
 import Header from '../Header/Header'
 import img from "./../../assets/Login/logo.png"
 
 export default function NextOrderingPage() {
+
+    const [count, setCount] = useState(1)
+    const Incre = () => {
+        setCount(count + 1)
+    }
+
+    const Decre = () => {
+        setCount(count - 1)
+    }
+
     return (
         <>
             <Header />
 
             <img src={img} alt="" className="invoice-logo" />
+
+            <div className='ORDERBG'>
+                <p className='ORDER'>Today Order</p>
+            </div>
             <div className='Npage'>
                 <h2 className='Next'>Shivkumar Loharkar</h2>
                 <h5>Number:8767234565</h5>
                 <h5>Address:Shrigonda</h5>
                 <h5>Email ID:shivkumar@gmail.com</h5>
                 <h2 className='Next'>Orders</h2>
+
                 <div className='NextOrder'>
                     <h4>Shirt :-</h4>
                     <div ><h5 className='Nprice'>180/-Per M</h5></div>
-                    <div className='fabric'></div>
+                    {/* <div className='fabric'></div> */}
+                    <h4>Color :-</h4>
+                    <div ><h5 className='Nprice'>Blue</h5></div>
+
+                    <div className='Oincrement'>
+                        <h4 className='Qty'>Qty:-</h4>
+                        <h3 onClick={Decre} >-</h3>
+                        <h2 className='count'>{count}</h2>
+                        <h3 onClick={Incre} >+</h3>
+                    </div>
+
+                    {/* <div className="incre">
+                        <h2 onClick={Decre} >-</h2>
+                    </div>
+                    <div className="incre name">
+                        <h2 >{count}</h2>
+                    </div>
+                    <h2 onClick={Incre} >+</h2>
+                     */}
                 </div>
 
                 <h2 className='Next'>Size</h2>
@@ -35,16 +68,28 @@ export default function NextOrderingPage() {
                     </div>
 
                 </div>
-                <h2 className='Next'>Rs.650</h2>
+                <h2 className='Next'>Descount</h2>
+                <div className='ODiscount'>
+                    <h5>Price:-<span className='Nspan'>₹ 600 </span></h5>
+                    <h5>Descount:-<span className='Nspan'>10%</span></h5>
+                    <h5>Total:-<span className='Nspan'>540</span></h5>
+                </div>
+
 
                 <div className='NLastpage'>
-                    <div className='LastN'>
+                    <button className='BOrder' >Edit Price</button>
+                    <button className='BOrder' >Edite Dis</button>
+                </div>
+
+
+                <div className='ONLastpage'>
+                    <button className='LastN'>
                         <h4 className='LastText'>Send</h4>
                         <h4 className='LastText'>8767899362</h4>
-                    </div>
-                    <div className='LastN'>
+                    </button>
+                    <button className='LastN'>
                         <h3 className='LastText'>Print Measurement</h3>
-                    </div>
+                    </button>
 
                 </div>
 
