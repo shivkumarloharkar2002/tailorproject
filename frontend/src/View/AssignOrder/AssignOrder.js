812d9235424ca412b048f664a04e7c2e1a102d2d
Bhoyate Dhanashree <bhoyatedhanashree5055@gmail.com>
2024-08-27 05:35:42 +0530

    add imgs

--- frontend/src/View/AssignOrder/AssignOrder.js
+++ frontend/src/View/AssignOrder/AssignOrder.js
@@ -5,49 +5,49 @@ import './Assign.css';
 export default function AssignOrder() {
   return (
     <div>
-      <Header/>
+      <Header />
       <p className='heding-assign'>Assign Orders</p>
       <div className='assign-main'>
         <div className='assign-left'>
-            <p>All</p>
-            <p>Shirt</p>
-            <p>Pant</p>
-            <p>Kurta</p>
-            <p>Pyjama</p>
-            <p>Sut Safari</p>
+          <p>All</p>
+          <p>Shirt</p>
+          <p>Pant</p>
+          <p>Kurta</p>
+          <p>Pyjama</p>
+          <p>Sut Safari</p>
         </div>
         <div className='assign-line'></div>
         <div>
-      <div className='assign-patti'>
-        <p>No</p>
-        <p>Date</p>
-        <p>Stutas</p>
-        <p>Order</p>
-        <p>Actions</p>
+          <div className='assign-patti'>
+            <p>No</p>
+            <p>Date</p>
+            <p>Stutas</p>
+            <p>Order</p>
+            <p>Actions</p>
+          </div>
+          <div className='assign-informetion'>
+            <p>1.</p>
+            <p>25/05/2024</p>
+            <p>Processing</p>
+            <p>Shirt</p>
+            <p>Unavailable</p>
+          </div>
+          <div className='assign-informetion'>
+            <p>2.</p>
+            <p>05/02/2024</p>
+            <p>Complete</p>
+            <p>Pant</p>
+            <p>Review</p>
+          </div>
+          <div className='assign-informetion'>
+            <p>3.</p>
+            <p>13/01/2024</p>
+            <p>Complete</p>
+            <p>Kurta</p>
+            <p>Review</p>
+          </div>
+        </div>
       </div>
-      <div className='assign-informetion'>
-        <p>1.</p>
-        <p>25/05/2024</p>
-        <p>Processing</p>
-        <p>Shirt</p>
-        <p>Unavailable</p>
-      </div>
-      <div className='assign-informetion'>
-        <p>2.</p>
-        <p>05/02/2024</p>
-        <p>Complete</p>
-        <p>Pant</p>
-        <p>Review</p>
-      </div>
-      <div className='assign-informetion'>
-        <p>3.</p>
-        <p>13/01/2024</p>
-        <p>Complete</p>
-        <p>Kurta</p>
-        <p>Review</p>
-      </div>
-      </div>
-    </div>
     </div>
   )
 }
