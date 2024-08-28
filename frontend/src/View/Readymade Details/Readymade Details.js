dd04a39d3fac0357aea0f7bbfd1a5257131446d4
Aniket <aniketsupekar2004@gmail.com>
2024-08-28 13:09:14 +0530

    readymade backend

--- frontend/src/View/Readymade Details/Readymade Details.js
+++ frontend/src/View/Readymade Details/Readymade Details.js
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import suts from './suts.webp';
 import './Ready.css';
+import axios from 'axios';
 import Header from '../../Component/Header/Header';
+import { useParams } from 'react-router-dom';
 
 export default function Ready() {
+
+  const {id} = useParams();
+  const [ReadyData, setReadyData] = useState([]);
+
+  const GetReadydata = async () => {
+    const readymade = await axios.get('http://localhost:5555/api/readymaderoutes/getreadymadedata');
+    setReadyData(readymade.data.data);
+    console.log(readymade.data)
+  }
+
+  useEffect(
+    () => {
+      GetReadydata();
+    }, []
+  )
+  console.log(ReadyData)
   return (
-    <div>
-        <Header/>
+    <>
+
+    {
+      ReadyData.map((data) => {
+        if(data._id ==id)
+          return(
+        
+        <>
+         <Header/>
         <p className='heding-ready'>Details</p>
     <div className='ready-flex'>
-      <img className='ready-img' src={suts}/>
+      <img className='ready-img' src={data.img}/>
       <div>
-        <h2 className='ready-head'>Wholesale Safari Suit for Men To Add Class To Every Man's Wardrobe</h2>
-        <p className='left-size'>Our safari suit is made from high-quality cotton fabric, ensuring comfort and durability.</p>
-        
-        <p className='left-size'>Available Sizes :- XS , S , M , L , XL , 2XL , 3XL .</p>
+        <h2 className='ready-head'>{data.title}</h2>
+        <p className='left-size'>{data.des}</p>
+        <p className='left-size'>Available Sizes :-{data.size}</p>
+       
+        <p className='left-size'>Sizes :-{data.size}</p>
         <div>
-        <p className='left-size'>Available Color :- </p>
-        <div className='colo'>
+        <p className='left-size'>Color :- {data.color}</p>
+        {/* <div className='colo'>
         <div className='rcolor'></div>
         <div className='ncolor'></div>
         <div className='gcolor'></div>
         <div className='bcolor'></div>
+        </div> */}
         </div>
-        </div>
-        <p className='left-size'>100% Original Products
-        Easy 14 days returns and exchanges .</p>
+        <p className='left-size'>{data.offer}</p>
+        <p className='left-size'> Price:-{data.price}</p>
         <div className='butns'>
           <p className='bts'>Buy Now</p>
         </div>
       </div>
     </div>
-    </div>
+        </>
+        
+        )
+      })
+    }
+       
+    </>
   )
 }
