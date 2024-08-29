1d160393bfbf51dcb190500e94c15242a8bb9142
Jacky2204 <jalindarbhapkar2@gmail.com>
2024-08-29 19:36:51 +0530

    date

--- frontend/src/Component/CustomerCard/SearchNameCard/SearchCard.js
+++ frontend/src/Component/CustomerCard/SearchNameCard/SearchCard.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import './SearchCard.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./SearchCard.css";
+import { Link } from "react-router-dom";
+import moment from "moment";
 // import { useNavigate } from "react-router-dom";
 // import axios from 'axios'
 
-
-export default function SearchCard({ name, date, price, id }) {
-
- 
+export default function SearchCard({ name, date, id }) {
+  const todaydate = moment(date).format("DD MMM YYYY");
 
   return (
     <>
-      <div className='invoice-shortCard'>
-        <Link to={`/viewcategory/${id}`} className='link'>
-          <div className="invoice-short" >
-            <p className="cust_info" >{name}</p>
-            <p className="invoice-short-amount">{date} <br />
+      <div className="invoice-shortCard">
+        <Link to={`/viewcategory/${id}`} className="link">
+          <div className="invoice-short">
+            <p className="cust_info">{name}</p>
+            <p className="invoice-short-amount">
+              {todaydate}
+              {/* <br />
               Paid- ₹ 
               {price}
-              /-
+              /- */}
             </p>
           </div>
         </Link>
       </div>
     </>
-  )
+  );
 }
--- frontend/src/View/Customer/ViewCustomers/ViewCustomer.js
+++ frontend/src/View/Customer/ViewCustomers/ViewCustomer.js
@@ -1,21 +1,16 @@
-import React from 'react'
-import './ViewCustomer.css'
-import customer from '../../../Image/people.png'
-import SearchCard from '../../../Component/CustomerCard/SearchNameCard/SearchCard'
-import { Link } from 'react-router-dom'
-import back from '../../../Image/back.jpg'
-import Header from '../../../Component/Header/Header'
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-
-
-
+import React from "react";
+import "./ViewCustomer.css";
+import customer from "../../../Image/people.png";
+import SearchCard from "../../../Component/CustomerCard/SearchNameCard/SearchCard";
+import { Link } from "react-router-dom";
+import back from "../../../Image/back.jpg";
+import Header from "../../../Component/Header/Header";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function ViewCustomer() {
-
   const [getCustomerData, setGetCustomerData] = useState([]);
-  const [search ,setSearch] = useState("") 
-
+  const [search, setSearch] = useState("");
 
   const getUser = async () => {
     const getNote = await axios.get(
@@ -25,52 +20,53 @@ export default function ViewCustomer() {
     console.log(getNote.data.data);
   };
 
-  const searchData = getCustomerData.filter(
-    (getcustomer)=>{
-        return(
-          getcustomer.name.toLowerCase().includes(search.toLowerCase())
-        )
-       
-    }
-)
- 
+  const searchData = getCustomerData.filter((getcustomer) => {
+    return getcustomer.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   useEffect(() => {
     getUser();
   }, []);
 
   return (
     <>
-      <header className='top_nav'><Header /></header>
+      <header className="top_nav">
+        <Header />
+      </header>
       <Link to={"/home"} className="link">
-        <div style={{margin:'60px 0 0 10px'}}>
+        <div style={{ margin: "60px 0 0 10px" }}>
           <img src={back} alt="" className="profile-back-img" />
           <h1 className="profile-back-text"></h1>
         </div>
       </Link>
-      <h1 style={{textAlign:'center' , marginBottom:'40px'}}>Old Customer</h1>
+      <h1 style={{ textAlign: "center", marginBottom: "40px" }}>
+        Old Customer
+      </h1>
       <div className="invoice-input">
         <img src={customer} alt="" className="invoice-icon" />
         <input
           type="text"
           className="invoice-inputs"
           placeholder="Search Customer Name"
-          onChange={
-            (e)=>{
-              setSearch(e.target.value)
-            }
-          }
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
         />
       </div>
 
-      <hr className='hr' />
+      <hr className="hr" />
 
       <div className="invoice-shortCards">
         {searchData.map((info) => {
-
           return (
             <>
               {/* <Link to='/seecustomer_details'> */}
-                < SearchCard name={info.name} date={info.date} price={info.price} id={info._id} />
+              <SearchCard
+                name={info.name}
+                date={info.createdAt}
+                // price={info.price}
+                id={info._id}
+              />
 
               {/* </Link>  */}
             </>
@@ -78,5 +74,5 @@ export default function ViewCustomer() {
         })}
       </div>
     </>
-  ) 
+  );
 }
