4820c3a106888f55c3ce87850f2d9c1e171644ae
kiranmadage <kiranmadage96@gmail.com>
2024-08-29 17:44:01 +0530

    checking

--- frontend/src/Component/GalleryCard/GalleryCard.js
+++ frontend/src/Component/GalleryCard/GalleryCard.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import './GalleryCard.css';
 // import NextGalleryCard from '../../View/Gallery/NextGalleryCard.js'
 
-export default function GalleryCard({ img, id }) {
+export default function GalleryCard({title, img, id }) {
   return (
     <>
       <Link to={`/NextGalleryCard/${id}`} className='link'>
 
 
         <div className="G_MAIN" >
+          <h2>{title}</h2>
           <img src={img} className="Img_height " alt="img" />
         </div>
 
