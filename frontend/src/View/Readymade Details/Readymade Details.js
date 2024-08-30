3274cde8d8ba0cae39433e03c9791103bfa8a12e
Bhoyate Dhanashree <bhoyatedhanashree5055@gmail.com>
2024-08-30 14:25:54 +0530

    Fabform

--- frontend/src/View/Readymade Details/Readymade Details.js
+++ frontend/src/View/Readymade Details/Readymade Details.js
@@ -7,7 +7,7 @@ import { useParams } from 'react-router-dom';
 
 export default function Ready() {
 
-  const {id} = useParams();
+  const { id } = useParams();
   const [ReadyData, setReadyData] = useState([]);
 
   const GetReadydata = async () => {
@@ -25,42 +25,42 @@ export default function Ready() {
   return (
     <>
 
-    {
-      ReadyData.map((data) => {
-        if(data._id ==id)
-          return(
-        
-        <>
-        
-         <Header/>
-        <p className='heding-ready'>Details</p>
-    <div className='ready-flex'>
-      <img className='ready-img' src={data.img}/>
-      <div>
-        <h2 className='ready-head'>{data.title}</h2>
-        <p className='left-size'>{data.des}</p>
-        <p className='left-size'>Sizes :-{data.size}</p>
-        <div>
-        <p className='left-size'>Color :- {data.color}</p>
-        {/* <div className='colo'>
+      {
+        ReadyData.map((data) => {
+          if (data._id == id)
+            return (
+
+              <>
+
+                <Header />
+                <p className='heding-ready'>Details</p>
+                <div className='ready-flex'>
+                  <img className='ready-img' src={data.img} />
+                  <div>
+                    <h2 className='ready-head'>{data.title}</h2>
+                    <p className='left-size'>{data.des}</p>
+                    <p className='left-size'>Sizes :-{data.size}</p>
+                    <div>
+                      <p className='left-size'>Color :- {data.color}</p>
+                      {/* <div className='colo'>
         <div className='rcolor'></div>
         <div className='ncolor'></div>
         <div className='gcolor'></div>
         <div className='bcolor'></div>
         </div> */}
-        
-        </div>
-        <p className='left-size'>{data.offer}</p>
-        <p className='left-size'> Price:-{data.price}</p>
-        
-      </div>
-    </div>
-        </>
-        
-        )
-      })
-    }
-       
+
+                    </div>
+                    <p className='left-size'>{data.offer}</p>
+                    <p className='left-size'> Price:-{data.price}</p>
+
+                  </div>
+                </div>
+              </>
+
+            )
+        })
+      }
+
     </>
   )
 }
