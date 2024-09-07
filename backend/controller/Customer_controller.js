75ef85ecd6e8d83b781b57c1141ce87d87ba921b
shiv <shivkumarloharkar2002@gmail.com>
2024-09-08 00:00:39 +0530

    cutomer add or check

--- backend/controller/Customer_controller.js
+++ backend/controller/Customer_controller.js
@@ -4,18 +4,27 @@ import Customer from "../models/customer_model.js"
 export const Customerregister = async (req, res) => {
     const Customer_id = "customerid" + Math.floor((Math.random() * 1000000))
     const { name, order_id, phone, measurement_id, email, address, fabric_id } = req.body
-    if (!name || !phone) {
-      return  ("please fill name, and phonenumber")
-    }
 
     try {
-        const customerdata = await Customer.create({
-            Customer_id, name, phone, address, email, measurement_id, order_id, fabric_id
-    })
-        res.json({
-            success: true, msg: "customer added",
-            data: customerdata
+        const OneD = await Customer.findOne({
+            name: name,
+            phone: phone
         })
+        if (OneD) {
+             res.json({
+                data: OneD,
+                msg: `${name} is already Registerd see on view customer `
+            })
+        }
+        else {
+            const customerdata = await Customer.create({
+                Customer_id, name, phone, address, email, measurement_id, order_id, fabric_id
+            })
+             res.json({
+                success: true, msg: "customer added",
+                data: customerdata
+            })
+        }
     }
     catch (e) {
         console.log(e)
@@ -38,7 +47,7 @@ export const Customerregister = async (req, res) => {
 
 export const AllCustomerdata = async (req, res) => {
     try {
-        const alldata =await Customer.find().populate("measurement_id").populate("order_id")
+        const alldata = await Customer.find().populate("measurement_id").populate("order_id")
         res.json({
             success: true,
             msg: "All customer Fetched",
@@ -50,27 +59,27 @@ export const AllCustomerdata = async (req, res) => {
     }
 }
 
-export const Onecustomer = async (req,res)=>{
-    const { name , phone } = req.body;
-    try{
-        
-        const OneD = await Customer.findOne({ 
-            name: name ,
-            phone:phone
-         })
-         if(OneD){
-            res.json({
-                msg:`${name} is already Registerd see on view customer `
-            })
-         }
-     else{
-        res.json({
-             Success:false,
-             msg:"Register Now"
+export const Onecustomer = async (req, res) => {
+    const { name, phone } = req.body;
+    try {
+
+        const OneD = await Customer.findOne({
+            name: name,
+            phone: phone
         })
-     }
+        if (OneD) {
+            res.json({
+                msg: `${name} is already Registerd see on view customer `
+            })
+        }
+        else {
+            res.json({
+                Success: false,
+                msg: "Register Now"
+            })
+        }
     }
-    catch(e){
+    catch (e) {
         console.log(e)
     }
 }
