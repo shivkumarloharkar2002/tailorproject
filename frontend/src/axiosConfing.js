5dd34f950e791e65c5359bf08d231e485448ce23
Mahesh Gore <144050192+mahigore@users.noreply.github.com>
2024-09-10 17:10:31 +0530

    Update axiosConfing.js

--- frontend/src/axiosConfing.js
+++ frontend/src/axiosConfing.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://apalabajar.com:5555/api',// Change this to your API base URL
+  baseURL: 'http://3.110.85.4:5555/api',// Change this to your API base URL
   withCredentials: true, // Include credentials in requests 
   //process.env.REACT_APP_BASE_URL ||
 }); 
